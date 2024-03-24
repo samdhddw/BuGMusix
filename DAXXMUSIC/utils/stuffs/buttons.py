3807002ea57c1a574c2c6fb8feaab1ba37004b04
˹ ɪ'ϻ ᴛ ᴏ x ɪ ᴄ ˼ ×͜™ <143880963+SH251204@users.noreply.github.com>
2024-03-24 07:44:46 +0000

    buttons.py

--- DAXXMUSIC/utils/stuffs/buttons.py
+++ DAXXMUSIC/utils/stuffs/buttons.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters, enums
 
 class BUTTONS(object):
     MBUTTON = [[InlineKeyboardButton("🍁CʜᴀᴛGPT🍁", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("🍁ɢʀᴏᴜᴘs🍁", callback_data="mplus HELP_Group"),InlineKeyboardButton("🍁🥀sᴛɪᴄᴋᴇʀs🍁🥀", callback_data="mplus HELP_Sticker")],
-    [InlineKeyboardButton("Tᴀɢ-Aʟʟ", callback_data="mplus HELP_TagAll"),
+    [InlineKeyboardButton("🍁Tᴀɢ-Aʟʟ🍁", callback_data="mplus HELP_TagAll"),
     InlineKeyboardButton("🍁Iɴꜰᴏ🍁", callback_data="mplus HELP_Info"),InlineKeyboardButton("🍁Exᴛʀᴀ🍁", callback_data="mplus HELP_Extra")],
     [InlineKeyboardButton("🍁Iᴍᴀɢᴇ🍁", callback_data="mplus HELP_Image"),
     InlineKeyboardButton("🍁Aᴄᴛɪᴏɴ🍁", callback_data="mplus HELP_Action"),InlineKeyboardButton("🍁Sᴇᴀʀᴄʜ🍁", callback_data="mplus HELP_Search")],    
